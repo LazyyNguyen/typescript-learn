4e14d9a91e5c1adeea86b67818e71d9b6d93d221
linhnguyen <linh.nguyen@stunited.vn>
2022-11-20 00:01:34 +0700

    prop types and tips

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import {Container} from './components/Container'
 function App() {
 
   return (
     <div className="App">
-      <Container styles={{border: '1px solid black'}}/>
     </div>
   );
 }
