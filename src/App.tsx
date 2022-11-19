532bda8d52890953e84de7892a2919b72f1144f6
linhnguyen <linh.nguyen@stunited.vn>
2022-11-19 17:49:28 +0700

    event props

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Status} from './components/Status';
-import {Heading} from './components/Heading';
-import {Oscar} from './components/Oscar';
-import {Greet} from './components/Greet';
-
+import {Button} from './components/Button';
+import {Input} from './components/Input';
 function App() {
   
   return (
     <div className="App">
-      <Status status='loading'/>
-      <Oscar>
-        <Heading>Placeholder text</Heading>
-      </Oscar>
-      <Greet name="Khanh Linh" isLoggedIn={true}/>
+      <Button handleClick={(event,id)=>{
+        console.log('Button clicked',event,id);
+      }}/>
+      <Input value='' handleChange={event => console.log(event)}/>
     </div>
   );
 }
