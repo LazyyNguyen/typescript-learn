3de9db89777485a3cfaa9e5003bc8e8ea5cce287
linhnguyen <linh.nguyen@stunited.vn>
2022-11-19 17:05:06 +0700

    basic props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
-import {Greet} from './components/Greet';
+import { Greet } from './components/Greet';
+import { Person } from './components/Person'
+import {PersonList} from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+  const personList = [
+    {
+      first: "Khánh",
+      last: "Linh"
+    },
+    {
+      first: "Linh",
+      last: "Nguyen"
+    },
+    {
+      first: "Khanh Linh",
+      last: "Nguyen Thi"
+    },
+
+  ]
   return (
     <div className="App">
-      <Greet name="Khánh Linh"/>
+      <Greet name="Khánh Linh" messageCount={3} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={personList}/>
     </div>
   );
 }
