019151277acc533d55a001427b53f5aa800cf237
linhnguyen <linh.nguyen@stunited.vn>
2022-11-22 16:51:46 +0700

    generics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+import { List } from './components/generics/List';
 function App() {
 
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile}/>
+      {/* <List
+        items={['Lemon', 'Apple', 'Banana', 'Watermelon']}
+        onClick={(item) => console.log(item)}
+      />
+      <List
+        items={[1, 2, 3, 4]}
+        onClick={(item) => console.log(item)}
+      /> */}
+            <List
+        items={[{id:0,name: 'Apple'},{id:1,name: 'Banana'},{id:2,name: 'Watermelon'}]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
